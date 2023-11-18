25d916696ba13073ef7cbf9962c1187335bd7988
JuFMacedo <julia_freitasmacedo@hotmail.com>
2023-11-17 23:08:52 -0300

    Criação Page Sobre e Readme

--- serrafy/App.js
+++ serrafy/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Sobre from "./src/pages/Sobre/sobre";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Sobre />
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
